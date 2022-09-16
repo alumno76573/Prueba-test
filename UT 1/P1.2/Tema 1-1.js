8404da2250940eb2bf4172c97c3f59e0bac142f5
alumno765731~ <alumno.76573@ies-azarquiel.es>
2022-09-16 21:06:39 +0200

    cambios

--- UT 1/P1.2/Tema 1-1.js
+++ UT 1/P1.2/Tema 1-1.js
@@ -1,1 +1,7 @@
 alert("Mensaje de alerta")
+console.log("Mensaje normal")
+console.info("Mensaje de informacion")
+console.warn("Mensaje de aviso")
+console.error("Mensaje de error")
+debugger
+console.log("Ha habido un punto de ruptura")
