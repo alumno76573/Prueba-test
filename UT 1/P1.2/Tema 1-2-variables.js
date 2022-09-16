9cc8e9715a5c6456f4aba034b6434f06403558a8
alumno765731~ <alumno.76573@ies-azarquiel.es>
2022-09-16 21:19:52 +0200

    guardar

---
+++ UT 1/P1.2/Tema 1-2-variables.js
@@ -0,0 +1,15 @@
+//variable definida de forma globar
+var Mensaje="Variable de tipo string"
+console.log(mensaje)
+var telefono="65832149"
+console.log(telefono)
+// variables según ecmascript6
+let nombre="Mi nombre"
+console.log(nombre)
+//constantes
+const apellido="Apellidos"
+console.log(apellido)
+//apellido="Segundo apellido"
+
+//Puedo definir una variable despues de haberla utilizado
+console.log(noDefinida)
