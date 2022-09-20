9fb25231ffe806b60d8a3c7ecc39446d0e395449
alumno765731~ <alumno.76573@ies-azarquiel.es>
2022-09-20 16:17:26 +0200

    func. anonimas

--- UT 1/Tema1/funciones/app.js
+++ UT 1/Tema1/funciones/app.js
@@ -10,4 +10,24 @@ function Suma(a,b){
     console.log(total)
 }
 Suma(2,3)
-console.log(total)
+//console.log(total)
+
+function otraNueva(name='coge el valor por defecto'){
+    console.log(name)
+}
+otraNueva('Toma el valor')
+otraNueva()
+
+function devuelveHola(texto='Hola'){
+    return texto+' Mundo'
+}
+console.log(devuelveHola())
+console.log(devuelveHola('Hello'))
+
+//FUNCIONES ANÓNIMAS
+
+var miNumero=function(numero=7){
+    return numero
+}
+console.log(miNumero())
+console.log(miNumero(89))
