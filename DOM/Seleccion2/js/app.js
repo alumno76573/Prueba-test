9b65624094b0e6d264a30a82c63117908db8fe5a
alumno765731~ <alumno.76573@ies-azarquiel.es>
2022-10-11 17:08:09 +0200

    bucles dom

--- DOM/Seleccion2/js/app.js
+++ DOM/Seleccion2/js/app.js
@@ -4,3 +4,16 @@ special.style.color="red"
 
 let lista_elementos=document.querySelectorAll("li")
 console.log(lista_elementos)
+
+//Diferentes bucles que podemos usar para aplicarle un estilo a cada item de un array
+
+/*for (let i=0;i<lista_elementos.length;i++){
+    lista_elementos[i].style.fontSize="2rem"
+}*/
+
+/*for (const item of lista_elementos){
+    item.style.fontSize="3rem"
+}*/
+lista_elementos.forEach(item=>{
+    item.style.fontSize="0.5rem"
+})
